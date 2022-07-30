7bc75ceec9173626a44770644e216dcbf787a520
Luis Mamani Lingan <72869012@certus.edu.pe>
2022-07-30 14:48:44 -0500

    port env

--- src/main.ts
+++ src/main.ts
@@ -29,6 +29,6 @@ async function bootstrap() {
   SwaggerModule.setup('docs', app, document);
 
   app.enableCors();
-  await app.listen(4000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
